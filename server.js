e9bb6efce3487d6d3945c6274740f77791afb2d3
mahadbajwa97 <mahadbajwa97@gmail.com>
2021-12-02 01:39:35 +0500

    feat: add base64 endpoint

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
+  if (req.url.match(/^\/b64\//)) return respondBase64(req, res)
+
 
   res.end()
 })
@@ -11,6 +13,11 @@ const server = http.createServer((req, res) => {
 function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
 }
+function respondBase64 (req, res) {
+  const phrase = req.url.replace(/^\/b64\//, '')
+  res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
+}
+
 
 server.listen(PORT)
 console.log(`Server listening on port ${PORT}`)
